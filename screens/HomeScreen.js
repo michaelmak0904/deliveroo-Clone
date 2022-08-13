d2dcfa075e47c037c8aac396acb6c90e5170abde
michael.mak <michaelmak0904@gmail.com>
2022-08-13 19:16:22 +0800

    building the restaurant screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,32 +1,86 @@
-import { View, Text, SafeAreaView, Image } from 'react-native'
-import React, { useLayoutEffect } from 'react'
+import { View, Text, SafeAreaView, Image, TextInput, ScrollView } from 'react-native'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
+import {
+    UserIcon,
+    ChevronDownIcon,
+    SearchIcon,
+    AdjustmentsIcon
+} from "react-native-heroicons/outline"
+import Categories from '../components/Categories'
+import FeaturedRow from '../components/FeaturedRow'
+import sanityClient from "../sanity"
+import category from '../sanity/schemas/category'
 
 const HomeScreen = () => {
     const navigation = useNavigation();
+    const [featuredCategories, setFeaturedCategories] = useState();
 
     useLayoutEffect(() => {
         navigation.setOptions({
             headerShown: false
         })
-    })
+    }, []);
+
+    useEffect(() => {
+        sanityClient.fetch(`
+        *[_type == "featured"] {
+            ...,
+            restaurants[]->{
+              ...,
+              dishes[]->
+            }
+          }`).then((data) => {
+            setFeaturedCategories(data)
+        })
+    }, [])
 
     return (
-        <SafeAreaView>
-            <Text className="text-red-500">
-                <View className="flex-row pb-3 items-center mx-4 space-x-2">
-                    <Image
-                        source={{ uri: 'https://links.papareact.com/wru' }}
-                        className="h-7 w-7 bg-gray-300 rounded-full"
-                    />
-                    <View>
-                        <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
-                        <Text className="font-bold text-xl">Current Location</Text>
-                    </View>
+        <SafeAreaView className="bg-white pt-5">
+            <View className="flex-row pb-3 items-center mx-4 space-x-2">
+                <Image
+                    source={{ uri: 'https://links.papareact.com/wru' }}
+                    className="h-7 w-7 bg-gray-300 rounded-full"
+                />
+                <View className="flex-1">
+                    <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+                    <Text className="font-bold text-xl">Current Location
+                        <ChevronDownIcon size={20} color="#00CCBB" />
+                    </Text>
                 </View>
-            </Text>
+                <UserIcon size={35} color="#00CCBB" />
+            </View>
 
+            {/* Search */}
+            <View className="flex-row items-center space-x-2 pb-2 mx-4">
+                <View className="flex-row space-x-2 flex-1 bg-gray-300 p-3">
+                    <SearchIcon color="gray" size={20} />
+                    <TextInput
+                        placeholder='Restaurants and cuisines'
+                        keyboardType='default'
+                    />
+                </View>
 
+                <AdjustmentsIcon color="#00CCBB" />
+            </View>
+            <ScrollView className="bg-gray-100"
+                contentContainerStyle={{
+                    paddingBottom: 100
+                }}>
+                <Categories />
+
+                {/* FeatureRow */}
+
+                {featuredCategories?.map((category) => (
+                    <FeaturedRow
+                        key={category._id}
+                        id={category._id}
+                        title={category.name}
+                        description={category.short_description}
+                    />
+                ))}
+
+            </ScrollView>
         </SafeAreaView>
     )
 }
